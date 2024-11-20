57c54c3c2581feb94aa4fd4db9aa822b59ab7514
Peter <pete.blinov@yandex.ru>
2024-11-20 12:38:02 +0100

    lint

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,15 @@
-import "@unocss/reset/tailwind.css";
-import "virtual:uno.css";
+import { Router } from '@solidjs/router'
+import { FileRoutes } from '@solidjs/start/router'
 
-import { Router } from "@solidjs/router";
-import { FileRoutes } from "@solidjs/start/router";
-import { Suspense } from "solid-js";
-import Nav from "~/components/Nav";
+import { Suspense } from 'solid-js'
+import Nav from '~/components/Nav'
+import '@unocss/reset/tailwind.css'
+import 'virtual:uno.css'
 
 export default function App() {
   return (
     <Router
-      root={(props) => (
+      root={props => (
         <>
           <Nav />
           <Suspense>{props.children}</Suspense>
@@ -18,5 +18,5 @@ export default function App() {
     >
       <FileRoutes />
     </Router>
-  );
+  )
 }
